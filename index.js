31e86739b8178ab6597ad08547ed8c8ed03e1425
znasriddinov <143064598+znasriddinov@users.noreply.github.com>
2023-10-24 19:54:12 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// Problem #210
+
+/*
+function toString(res) {
+  res = parseInt(155);
+
+  console.log(res.toString(2));
+  console.log(res.toString(8));
+  console.log(res.toString(16));
+}
+
+toString();
+*/
+
